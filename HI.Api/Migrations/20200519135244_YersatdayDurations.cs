eecf0030c152d63c0749816c49f6283b8bd211f1
victor.borz <victorborz@prpl.global>
2020-05-20 02:36:56 +0500

    migration

---
+++ HI.Api/Migrations/20200519135244_YersatdayDurations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HI.Api.Migrations
+{
+    public partial class YersatdayDurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "YesterdayDuration",
+                table: "Histories",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YesterdayDuration",
+                table: "Histories");
+        }
+    }
+}
---
+++ HI.Api/Migrations/20200519142958_TotalDurations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HI.Api.Migrations
+{
+    public partial class TotalDurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TotalDuration",
+                table: "Histories",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDuration",
+                table: "Histories");
+        }
+    }
+}
